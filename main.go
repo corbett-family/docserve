a5bfb71e0e1f526637f8995fb925e23e3a348acb
Jason Corbett <jasoncorbett@gmail.com>
2023-10-19 16:05:32 -0600

    Now handles single page application, and no cache on swagger.json

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 //go:embed ui/public
@@ -28,13 +29,87 @@ func main() {
 		os.Exit(1)
 	}
 
+	var root, _ = fs.Sub(content, "ui/public")
+	httpFS := http.FS(root)
+	fileServer := http.FileServer(httpFS)
+	serveIndex := serveFileContents("index.html", httpFS)
+
 	http.HandleFunc("/swagger.json", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-cache")
 		http.ServeFile(w, r, "/Users/jcorbett/dev/partner-api/src/tsoa/swagger.json")
 	})
 
-	var root, _ = fs.Sub(content, "ui/public")
-	http.Handle("/", http.FileServer(http.FS(root)))
+	http.Handle("/", intercept404(fileServer, serveIndex))
 
 	println(fmt.Sprintf("Docs available at http://localhost:%d", *port))
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
+}
+
+// the following comes from https://hackandsla.sh/posts/2021-11-06-serve-spa-from-go/
+// to handle a single page application
+
+type hookedResponseWriter struct {
+	http.ResponseWriter
+	got404 bool
+}
+
+func (hrw *hookedResponseWriter) WriteHeader(status int) {
+	if status == http.StatusNotFound {
+		// Don't actually write the 404 header, just set a flag.
+		hrw.got404 = true
+	} else {
+		hrw.ResponseWriter.WriteHeader(status)
+	}
+}
+
+func (hrw *hookedResponseWriter) Write(p []byte) (int, error) {
+	if hrw.got404 {
+		// No-op, but pretend that we wrote len(p) bytes to the writer.
+		return len(p), nil
+	}
+
+	return hrw.ResponseWriter.Write(p)
+}
+
+func intercept404(handler, on404 http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		hookedWriter := &hookedResponseWriter{ResponseWriter: w}
+		handler.ServeHTTP(hookedWriter, r)
+
+		if hookedWriter.got404 {
+			on404.ServeHTTP(w, r)
+		}
+	})
+}
+
+func serveFileContents(file string, files http.FileSystem) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Restrict only to instances where the browser is looking for an HTML file
+		if !strings.Contains(r.Header.Get("Accept"), "text/html") {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprint(w, "404 not found")
+
+			return
+		}
+
+		// Open the file and return its contents using http.ServeContent
+		index, err := files.Open(file)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(w, "%s not found", file)
+
+			return
+		}
+
+		fi, err := index.Stat()
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(w, "%s not found", file)
+
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		http.ServeContent(w, r, fi.Name(), fi.ModTime(), index)
+	}
 }
